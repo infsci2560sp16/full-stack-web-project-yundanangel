e66a56553dd2fa2a96f8e286c150c9101ab44996
yundanangel <yundanangel@outlook.com>
2016-03-30 23:55:26 -0400

    hahaha

---
+++ src/main/java/HtmlContent.java
@@ -0,0 +1,34 @@
+
+
+public class HtmlContent {
+  
+    private String userName;   
+    private String userPassword;   
+    /**  
+     * @return the userName  
+     */  
+    public String getUserName() {   
+        return userName;   
+    }   
+    /**  
+     * @param userName the userName to set  
+     */  
+    public void setUserName(String userName) {   
+        this.userName = userName;   
+    }   
+    /**  
+     * @return the userPassword  
+     */  
+    public String getUserPassword() {   
+        return userPassword;   
+    }   
+    /**  
+     * @param userPassword the userPassword to set  
+     */  
+    public void setUserPassword(String userPassword) {   
+        this.userPassword = userPassword;   
+    }   
+  
+  
+}  
+
